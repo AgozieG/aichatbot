4c198906610c0c4b1a253da722b979168f2de0de
Iwunna George <georgeiwunna@gmail.com>
2026-02-10 09:59:14 +0100

    hey00

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugin: [react()],
-    base: "/aichatbot",
+    plugins: [react()],
+    base: "/aichatbot/",
 });
